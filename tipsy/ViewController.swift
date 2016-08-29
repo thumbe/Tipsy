6a1802434a10d2578d95080daa6860b8e409341c
Tushar Humbe <tushum@yahoo-inc.com>
2016-08-28 19:22:49 -0700

    Initial Commit

---
+++ tipsy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipsy
+//
+//  Created by Tushar Humbe on 8/28/16.
+//  Copyright © 2016 Tushar Humbe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
