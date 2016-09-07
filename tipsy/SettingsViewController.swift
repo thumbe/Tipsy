c92c53dc928ece39fc6771e027b24f8ad8dc49e6
Tushar Humbe <tushum@yahoo-inc.com>
2016-09-07 11:25:04 -0700

    add settings view controller

---
+++ tipsy/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  tipsy
+//
+//  Created by Tushar Humbe on 9/7/16.
+//  Copyright © 2016 Tushar Humbe. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+}
--- tipsy/ViewController.swift
+++ tipsy/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billLabel: UITextField!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var tipTextBox: UITextField!
+    @IBOutlet weak var tipSelector: UISegmentedControl!
+    
+    var existingTip: Double = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        //tipSelector.setTitle("20%", forSegmentAtIndex: 0);
+        //tipSelector.setTitle("30%", forSegmentAtIndex: 1);
+        //tipSelector.setTitle("40%", forSegmentAtIndex: 2);
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +37,50 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    
+    @IBAction func onAmountChanged(sender: AnyObject) {
+        if (tipSelector.selectedSegmentIndex >= 0) {
+            if (tipSelector.selectedSegmentIndex == 3) {
+                
+                tipTextBox.hidden = false
+                tipLabel.hidden = true
+                tipTextBox.text = String.init(format: "%.2f", existingTip);
+                tipTextBox.becomeFirstResponder()
+                return
+            } else {
+                tipTextBox.hidden = true
+                tipLabel.hidden = false
+                billLabel.becomeFirstResponder()
+                
+            }
+        let tipValues = [0.15, 0.18, 0.20, 0.25]
+        
+        let bill = Double(billLabel.text!) ?? 0
+        let tip = bill * tipValues[tipSelector.selectedSegmentIndex];
+            existingTip = tip;
+        let total = bill + tip;
+        
+        tipLabel.text = String.init(format: "$%.2f", tip)
+        amountLabel.text = String.init(format: "$%.2f", total)
+        }
+        
+    }
+    @IBAction func onTipEntered(sender: AnyObject) {
+        
+        let bill = Double(billLabel.text!) ?? 0
+        
+        let tip = Double(tipTextBox.text!) ?? 0
+        
+        let total = tip + bill
+        tipLabel.text = String.init(format: "$%.2f", tip)
+        amountLabel.text = String.init(format: "$%.2f", total)
+        
+    }
+    
+    
 }
 
