66579ef445953993a52631f85fecb877996514df
Tushar Humbe <tusharhumbe@Nikus-iMac.local>
2016-09-27 22:47:32 -0700

    adding extensions: persist amount, firstResponder

--- tipsy/ViewController.swift
+++ tipsy/ViewController.swift
@@ -10,7 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    static let PREVIOUS_BILL_AMOUNT_KEY = "PREVIOUS_BILL_AMOUNT"
+    static let PREVIOUS_BILL_AMOUNT_TIME_KEY = "PREVIOUS_BILL_AMOUNT_TIME"
+    // time in milliseconds for which the bill amount is saved between app restarts
+    let billPersistentTimeMS: Double = 10*1000;
     @IBOutlet weak var tipLabel: UILabel!
+    
+    // TODO: refractor to someting billTextField. Why cannot we refactor in swift?! :o
     @IBOutlet weak var billLabel: UITextField!
     @IBOutlet weak var amountLabel: UILabel!
     @IBOutlet weak var tipTextBox: UITextField!
@@ -21,6 +27,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        billLabel.becomeFirstResponder()
         
     }
     
@@ -36,7 +43,6 @@ class ViewController: UIViewController {
         
         let defaults = UserDefaults.standard
         
-         UIImageView(frame: CGRect(x: 0, y: 20, width: 320, height: 320))
         imageView.image = UIImage(named: "manutdLogo.png")
         self.view.insertSubview(imageView, at: 0)
         
@@ -50,9 +56,24 @@ class ViewController: UIViewController {
             imageView.removeFromSuperview()
         }
         
+        // set any previous bill amount if we have any within certain time.
+        if ((NSDate().timeIntervalSince1970 * 1000) - defaults.double(forKey: ViewController.PREVIOUS_BILL_AMOUNT_TIME_KEY) < billPersistentTimeMS) {
+            billLabel.text = defaults.string(forKey: ViewController.PREVIOUS_BILL_AMOUNT_KEY)
+        } else {
+            billLabel.text = ""
+        }
+        
         let intValue = defaults.integer(forKey: SettingsViewController.DEFAULT_VALUE_KEY);
         
         tipSelector.selectedSegmentIndex = intValue;
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        // save current bill amount and time in miliseconds
+        UserDefaults.standard.set(billLabel.text, forKey: ViewController.PREVIOUS_BILL_AMOUNT_KEY)
+        print("viewWillDisappear: "+String(NSDate().timeIntervalSince1970))
+        UserDefaults.standard.set(NSDate().timeIntervalSince1970 * 1000, forKey: ViewController.PREVIOUS_BILL_AMOUNT_TIME_KEY)
+        UserDefaults.standard.synchronize()
     }
 
     override func didReceiveMemoryWarning() {
@@ -90,6 +111,7 @@ class ViewController: UIViewController {
         
         tipLabel.text = String.init(format: "$%.2f", tip)
         amountLabel.text = String.init(format: "$%.2f", total)
+            
         }
         
     }
